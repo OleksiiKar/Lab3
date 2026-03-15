022fb6bf26d98345d5b40c76c5796fb0f683fa29
OleksiiKar <oleksiykar@icloud.com>
2026-03-15 14:42:16 +0200

    Update Main.java

--- src/main/java/Lab3/Main.java
+++ src/main/java/Lab3/Main.java
@@ -1,7 +1,8 @@
 package Lab3;
+import Lab3.Airplanes;
 
 public class Main {
     static void main() {
-
+        Airplanes[] garage = {new Airplanes("AH-225", "Antonov", 800, 12000, 250)};
     }
 }
